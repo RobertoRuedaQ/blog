b36d1b42cbc6a911088615f9f7216a06a262a885
roberto <rueda.roberto89@gmail.com>
2017-05-13 14:42:04 -0500

    added comments, delete comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  has_many :comments, dependent: :destroy 
+
   validates :name, presence: true
   validates :title, presence: true
   validates :body, presence: true, length: {minimum: 250}
