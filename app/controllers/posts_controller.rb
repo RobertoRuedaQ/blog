c25ee7b71daa276f9d5fd8cea66e4e9161d2ea01
roberto <rueda.roberto89@gmail.com>
2017-05-13 11:59:09 -0500

    updated the style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,20 +4,33 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
    @post = Post.new(post_params)
-   @post.save
+
+   if @post.save
    redirect_to @post
+   else
+     render "new"
+   end
+
   end
 
   def show
   	@post = Post.find(params[:id])
   end
 
+  def destroy
+   @post = Post.find(params[:id])
+   @post.destroy
+
+   redirect_to :index, alert: "the post has been deleted"
+  end
+
   private
   def post_params
-  	params.require(:post).permit(:title, :body)
+  	params.require(:post).permit(:name, :title, :body)
   end
 end
